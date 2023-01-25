03d0183ac69325b84da73b15d3ec5126cb130cf4
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-25 15:30:12 -0300

    style: remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
